dfa34cda419b1c9bd0b0a22b0a7a4710a0fa477e
Blazing-Blast <lukabijma1@gmail.com>
2022-12-22 20:00:37 +0100

    The slingshot works now
    Fixed all the bugs found in the review.
    Added code to translate px to %, it also unintentionally made the aiming smoother which may actually be better.

--- Scripts/slingshot_3d.gd
+++ Scripts/slingshot_3d.gd
@@ -14,7 +14,7 @@ func Initialize():
 func LoadAmmo(ammo):
 	current_ammo = ammo
 	# Moves the ammo to the centre and connects the strings to it
-	ammo_position = %Centre.position
+	ammo_position = Vector2 (%Centre.position.x, %Centre.position.y)
 	current_ammo.position = %Centre.position
 	move_ammo()
 
@@ -33,19 +33,19 @@ func fire():
 	
 	# Applies a impulse with X and Y equal to the oposite of the distance to the centre
 	# and Z (forward) equal to the total stretch of the string
-	current_ammo.apply_central_impulse(Vector3(%Centre.position.x - ammo_position.x, %Centre.position.y - ammo_position.y, Vector2(%Centre.position.x - ammo_position.x, %Centre.position.y - ammo_position.y).length()))
+	current_ammo.freeze = false
+	current_ammo.apply_central_impulse(Vector3(%Centre.position.x - ammo_position.x, %Centre.position.y - ammo_position.y, -Vector2(%Centre.position.x - ammo_position.x, %Centre.position.y - ammo_position.y).length()))
 	
 	# Cleans up to get ready for the next reload
-	current_ammo = null
 	armed = false
-	ammo_position = %Centre.position
+	ammo_position = Vector2 (%Centre.position.x, %Centre.position.y)
 	move_ammo()
+	current_ammo = null
 	emit_signal("reload")
 
 # Check if the mouse is released, then fire
 func _input(event):
 	if current_ammo == null: return
-	print("Input: ", event.to_string())
 	if event is InputEventMouseButton:
 		print("We just pressed a button: ", event.to_string())
 		if armed and not event.is_pressed():
@@ -54,13 +54,18 @@ func _input(event):
 		elif not armed and event.is_pressed():
 			armed = true
 	elif event is InputEventMouseMotion and armed:
+		
+		
+		
 		# Moved the ammo to the mouse, but not outside or above the corners or the catapult and not more than 400px below the centre
-		ammo_position = event.position.clamp(Vector2(%LeftEnd.position.x, %Centre.position.y - 15), %RightEnd.position)
+		var input_position = px_to_percent(event.position) * 20
+		input_position = Vector2(input_position.x, -input_position.y)
+		ammo_position = input_position.clamp(Vector2(%Left.position.x, %Centre.position.y - 15), Vector2 (%Right.position.x, %Right.position.y))
 		move_ammo()
+
+func px_to_percent(original: Vector2) -> Vector2:
+	return Vector2(original.x / get_viewport().get_visible_rect().size.x - 0.5, original.y / get_viewport().get_visible_rect().size.y - 0.5)
 
 # Moves the ammo and strings to where they should be
 func move_ammo():
-	if current_ammo.position == null:
-		print("Aborted moving ammo because it is null")
-		return
 	current_ammo.position = Vector3(ammo_position.x, ammo_position.y, %Centre.position.z)
