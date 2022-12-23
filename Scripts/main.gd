eabd6b016526a45650b9bb5661643bbc823c963f
Blazing-Blast <lukabijma1@gmail.com>
2022-12-23 13:21:03 +0100

    Fixed the testing AGAIN

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,7 +1,11 @@
-extends Node3D
-var loaded = false
+extends Node
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$slingshot_3d.Initialize()
-	$slingshot_3d.LoadAmmo($ammo_placeholder)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
